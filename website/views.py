03f0ea935691f12272d4980c1c92ab191cf72451
David Buchmann <david@david-buchmann.de>
2024-01-06 15:23:21 +0100

    Added delete function and improved visual presentation of tasks

--- website/views.py
+++ website/views.py
@@ -38,35 +38,42 @@ def task():
 @views.route('/add-task', methods=['POST'])
 def add_task():
     #task = request.form['todo']
-    task = request.form.get('todo')
-    if len(task) < 1:
-        flash('Task is too short!', category='error')
-    else:
+    task = request.form.get('task')
+    #if len(task) < 1:
+    #    flash('Task is too short!', category='error')
+    #else:
         #user.tasks.append({'todo': task, 'done': False})
-        new_task = Task(data=task, user_id=current_user.id)
-        db.session.add(new_task)
-        db.session.commit()
-        flash('Task added!')
+    new_task = Task(data=task, user_id=current_user.id)
+    db.session.add(new_task)
+    db.session.commit()
+    flash('Task added!')
     
     return redirect(url_for("views.task"))
 
-@views.route('/edit/<int:index>', methods=['GET', 'POST'])
-def edit_tasks(index):
-    task = tasks[index]
+@views.route('/edit/<int:taskId>', methods=['GET', 'POST'])
+def edit_tasks(taskId):
+    task = json.loads(request.data)
+    taskId = task['taskId']
+    task = Task.query.get(taskId)
     if request.method == "POST":
-        task['todo'] = request.form['task']
+        task.data = request.form['task']
         return redirect(url_for("views.task"))
     else:
-        return render_template("edit.html", task=task, index=index, user=current_user)
+        return render_template("edit.html", task=task, user=current_user)
     
-@views.route('/check/<int:index>')
-def check_tasks(index):
-    tasks[index]['done'] = not tasks[index]['done']
+@views.route('/check/<int:taskId>')
+def check_tasks(taskId):
+    task.done = not task.done
     return redirect(url_for("views.task"))
 
-@views.route('/delete/<int:index>')
-def delete_tasks(index):
-    del tasks[index]
+@views.route('/delete/<int:taskId>')
+def delete_tasks(taskId):
+    task = Task.query.get(taskId)
+    if task:
+        if task.user_id == current_user.id:
+            db.session.delete(task)
+            db.session.commit()
+    
     return redirect(url_for("views.task"))
 
 @views.route('/delete-note', methods=['POST'])
