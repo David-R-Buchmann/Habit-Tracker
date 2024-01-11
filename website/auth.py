ed3e5621ff39f3edcc0a19017f175ac04b38f0ec
David Buchmann <david@david-buchmann.de>
2024-01-11 19:25:11 +0100

    Added some comments

--- website/auth.py
+++ website/auth.py
@@ -6,48 +6,49 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods=['GET', 'POST'])
+# Contains all available paths concerining the authentification of user login and signup.
+
+@auth.route('/login', methods=['GET', 'POST']) # defines /login as the route for logging in.
 def login():
     if request.method == 'POST':
-        userName = request.form.get('userName')
-        password = request.form.get('password1')
-        print(password)
+        userName = request.form.get('userName') # gets the username from the request form's corresponding input field
+        password = request.form.get('password1') # gets the password from the request form's corresponding input field (only password1 since password1 and password2 need to be identical)
 
-        user = User.query.filter_by(userName=userName).first()
+        user = User.query.filter_by(userName=userName).first() # searches for the user amongst all User instances and selects the first entry found
         if user: 
-            if check_password_hash(user.password, password):
+            if check_password_hash(user.password, password): # checks for the hashed password
                 flash('Logged in successfully!', category='success')
-                login_user(user, remember=True)
+                login_user(user, remember=True) # remembers the user's login
                 return redirect(url_for('views.home'))
             else: 
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('User Name does not exist.', category='error')
 
-    return render_template("login.html", user=current_user)
+    return render_template("login.html", user=current_user) # returns the login screen.
 
-@auth.route('/logout')
+@auth.route('/logout') # logs out the user under the route /logout
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
+@auth.route('/sign-up', methods=['GET', 'POST']) # defines the signup route /sign-up
 def sign_up():
-    if request.method == 'POST':
-        userName = request.form.get('userName')
+    if request.method == 'POST': 
+        userName = request.form.get('userName') # gets all necessary information from the request forms
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        user = User.query.filter_by(userName=userName).first()
+        user = User.query.filter_by(userName=userName).first() # searches for the user amongst all User instances and selects the first entry found
         if user:
             flash('User Name already exists.', category='error')
-        elif len(userName) < 4:
+        elif len(userName) < 4: # username must be at least 4 characters long
             flash('User Name must be longer than 3 characters.', category='error')
-        elif len(first_name) < 2:
+        elif len(first_name) < 2: # first name must be at least 2 characters long
             flash('First name must be longer than 1 characters.', category='error')
-        elif password1 != password2:
+        elif password1 != password2: # passwords must match
             flash('Passwords do not match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at leaast 7 characters.', category='error')
